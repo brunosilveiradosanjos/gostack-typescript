415f6723ff5d905280cd21b662f9747aacc933b3
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-08-27 21:10:46 -0300

    Tipando Objetos e Vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(req: Request, res: Response) {
+    const user = createUser({
+        email: 'bruanjo@gmail.com',
+        password: 'JoNeiOh*ˆB@08s',
+        techs: ['Node.js',
+            'React.js',
+            'React Native',
+            { title: 'JS', experience: 10 },
+        ]
+    });
     return res.json({ message: 'Hello World' })
 }
---
+++ src/services/CreateUser.ts
@@ -0,0 +1,22 @@
+
+interface TechObejct {
+    title: string;
+    experience: number;
+}
+
+interface CreateUserData {
+    name?: string;
+    email: string;
+    password: string;
+    techs: Array<string | TechObejct>
+}
+
+export default function createUser({ name, email, password, techs }: CreateUserData) {
+    const user = {
+        name,
+        email,
+        password,
+        techs
+    }
+    return user;
+}
